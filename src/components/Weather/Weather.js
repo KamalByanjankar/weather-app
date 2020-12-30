01583735fee527844a4623a8045276402121fe49
Kamal Byanjanakr <kamalbyanjankar@gmail.com>
2020-12-30 21:08:05 +0100

    api data fetch

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+
+const url = "http://api.openweathermap.org/data/2.5/weather?q=Frankfurt&appid=2c190e833a80ec82eb4bf050a1da5404&units=metric"
 
 class Weather extends Component {
-    state = {  }
+    state = { 
+        weather: {}
+     }
 
     dateBuilder = (d) => {
         let months= ["January", "February", "March", "April", "May", "June", "July",
@@ -12,29 +18,41 @@ class Weather extends Component {
         let date = d.getDate();
         let month = months[d.getMonth()];
         let year = d.getFullYear();
-
         return `${day} ${date} ${month} ${year}`
     }
 
+    componentDidMount(){
+        axios.get(url)
+            .then(response => {
+                // console.log(response)
+                this.setState({
+                    weather: response.data
+                })
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
     render() { 
+        const weather = this.state.weather;
         return ( 
             <div>
-                <div>
-                    <p>Frankfurt am Main, DE</p>
-                    <p>{this.dateBuilder(new Date())}</p>
-                </div>
-                <div>
-                    <p>Partly Cloudy</p>
-                    <p>6 &deg;</p>
-                </div>  
-                <div>
-                    <p>High: 5 &deg;</p>
-                    <p>Low: 1 &deg;</p>
-                    <p>Sunrise</p>
-                    <p>08:24</p>
-                    <p>Sunset</p>
-                    <p>16:32</p>
-                </div>
+                {(typeof weather.main != 'undefined') ? 
+                    <div>
+                        <div>
+                            <p>{weather.name}, {weather.sys.country}</p>
+                            <p>{this.dateBuilder(new Date())}</p>
+                        </div>
+                        <div>
+                            <p>{weather.weather[0].main}</p>
+                            <p>{Math.round(weather.main.temp)}&deg;</p>
+                        </div>  
+                        <div>
+                            <p>High: {Math.round(weather.main.temp_max)}&deg;</p>
+                            <p>Low: {Math.round(weather.main.temp_min)}&deg;</p>
+                        </div>
+                    </div>  : ('')}             
             </div>
         );
     }
