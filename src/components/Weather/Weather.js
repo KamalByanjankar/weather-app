acab465aed678549e36cd1744c00b608c1b09a21
Kamal Byanjanakr <kamalbyanjankar@gmail.com>
2021-01-04 13:53:28 +0100

    Small change

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -42,6 +42,7 @@ class Weather extends Component {
         let minutes = date.getMinutes();
 
         let formattedTime = hours + ':' + minutes;
+        // console.log(formattedTime)
         return formattedTime;
     }
 
@@ -55,7 +56,7 @@ class Weather extends Component {
         this.setState({
             weather: data
         })
-        // console.log(this.state.weather)
+        console.log(this.state.weather)
     }
 
     render() {
